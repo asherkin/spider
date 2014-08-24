3d76624dc257d183624bddc64855da2600cc2315
Asher Baker <asherkin@gmail.com>
2014-08-24 14:45:48 +0100

    Added new buttons to translation files.

--- src/js/translations/fr.js
+++ src/js/translations/fr.js
@@ -1,10 +1,13 @@
-// Arkshine
+// Arkshine, ANTICHRISTUS
 translations['fr'] = [
   "Sorry, your browser is not supported.",
   "Spider a été mis à jour.",
   "Rafraîchir la page pour charger la nouvelle version.",
   "Votre dernière session a été restaurée.",
   "Repartir de zéro à la place?",
+  "Clair",
+  "Sombre",
+  "Plein Écran",
   "Compiler",
   "Télécharger",
   "Déposer des fichiers", "ici",
