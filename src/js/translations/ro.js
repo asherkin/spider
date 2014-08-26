f70a24ef27318c5df917790ba4994bf410e54536
Asher Baker <asherkin@gmail.com>
2014-08-26 19:07:56 +0100

    Add Romanian translation.

---
+++ src/js/translations/ro.js
@@ -0,0 +1,16 @@
+// HamletEagle
+translations['ro'] = [
+  "Din pacate browserul tau nu este suportat.",
+  "Spider a fost updatat.",
+  "Reincarca pagina pentru a vedea noua versiune.",
+  "Ultima ta sesiune a fost restaurata.",
+  "Vrei o sesiune noua?",
+  "Luminos",
+  "Intunecat",
+  "Pe tot ecranul",
+  "Compileaza",
+  "Descarca",
+  "Pune", "fisierele aici",
+  "Creat de",
+  "Powered by", "si",
+];
