4af335db092810a165bc8ee27c1b3115c2f6bb8c
B3none <ablackham2000@gmail.com>
2019-03-29 01:52:39 +0000

    Altered electron file

--- electron.js
+++ electron.js
@@ -4,41 +4,41 @@ const {app, BrowserWindow} = electron;
 let win;
 
 const createWindow = () => {
-    win = new BrowserWindow({
-        title: 'Spider - SourcePawn Compiler',
-        backgroundColor: '#272822',
-        show: false
-    });
+  win = new BrowserWindow({
+    title: 'Spider - SourcePawn Compiler',
+    backgroundColor: '#272822',
+    show: false
+  });
 
-    win.loadFile('index.html');
+  win.loadFile('index.html');
 
-    let eventCount = 0;
-    let processEvent = () => {
-        if (++eventCount === 2) {
-            win.maximize();
-            win.show();
-            win.focus();
-        }
-    };
+  let eventCount = 0;
+  let processEvent = () => {
+    if (++eventCount === 2) {
+      win.maximize();
+      win.show();
+      win.focus();
+    }
+  };
 
-    win.webContents.on('did-finish-load', processEvent);
-    win.on('ready-to-show', processEvent);
+  win.webContents.on('did-finish-load', processEvent);
+  win.on('ready-to-show', processEvent);
 
-    win.on('closed', () => {
-        win = null;
-    })
+  win.on('closed', () => {
+    win = null;
+  })
 };
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-    if (win === null) {
-        createWindow();
-    }
+  if (win === null) {
+    createWindow();
+  }
 });
