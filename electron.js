d10db39192825101a3e7ce1b15666296d6eb293c
B3none <ablackham2000@gmail.com>
2019-03-29 00:44:55 +0000

    Updated electron config

--- electron.js
+++ electron.js
@@ -7,8 +7,7 @@ let win;
 
 const createWindow = () => {
     // Create the browser window.
-    win = new BrowserWindow();
-
+    win = new BrowserWindow({show: false});
     win.maximize();
 
     // and load the index.html of the app.
@@ -17,12 +16,16 @@ const createWindow = () => {
     // Open the DevTools.
     win.webContents.openDevTools();
 
+    win.webContents.on('ready-to-show', () => {
+        win.show();
+    });
+
     // Emitted when the window is closed.
     win.on('closed', () => {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        win = null
+        win = null;
     })
 };
 
@@ -36,7 +39,7 @@ app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
 });
 
@@ -44,7 +47,7 @@ app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (win === null) {
-        createWindow()
+        createWindow();
     }
 });
 
